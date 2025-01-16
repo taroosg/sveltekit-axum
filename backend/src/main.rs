04c5caf140f1ad3745ba1a5ec1362b0d2da9f59b
taroosg <root@taroosg.dev>
2025-01-16 14:25:22 +0900

    プロジェクト作成

---
+++ backend/src/main.rs
@@ -0,0 +1,16 @@
+// backend/src/main.rs
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("Listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
